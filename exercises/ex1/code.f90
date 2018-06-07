5fff19b6cc3623b4caf02892460c2846ec7e500d
Valentin Clement <clementval@gmail.com>
2018-06-07 17:02:38 +0200

    Add ex1

---
+++ exercises/ex1/code.f90
@@ -0,0 +1,23 @@
+program ex1
+  implicit none
+  integer, parameter :: n = 10
+  real(8) :: a(n)
+  real(8) :: b(n)
+  integer :: i
+
+  do i=1,n
+    print*,'loop 1 iteration: ',i
+  end do
+
+  do i=1,n
+    print*,'loop 2 iteration: ',i
+  end do
+
+  do i=1,n
+    print*,'loop 1 iteration: ',i
+  end do
+
+  do i=1,n
+    print*,'loop 1 iteration: ',i
+  end do
+end program ex1
