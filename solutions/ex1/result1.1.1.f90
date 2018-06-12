498e1d0c74b21323213dce0f4a4c7a360171f2c0
Valentin Clement <clementval@gmail.com>
2018-06-12 10:39:37 +0200

    Add result transformation

---
+++ solutions/ex1/result1.1.1.f90
@@ -0,0 +1,14 @@
+PROGRAM ex1
+ INTEGER , PARAMETER :: n = 10
+ REAL ( KIND= 8 ) :: a ( 1 : 10 )
+ REAL ( KIND= 8 ) :: b ( 1 : 10 )
+ INTEGER :: i
+
+ DO i = 1 , 10 , 1
+  PRINT * ,"loop 1 iteration: " , i
+  PRINT * ,"loop 2 iteration: " , i
+  PRINT * ,"loop 3 iteration: " , i
+  PRINT * ,"loop 4 iteration: " , i
+ END DO
+END PROGRAM ex1
+
